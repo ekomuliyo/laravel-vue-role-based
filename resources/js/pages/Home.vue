699561991fba8b3542aefd7d047fc7f941d020c5
eko <ekomuliyo@gmail.com>
2021-08-12 20:47:39 +0700

    initial project

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <div class="card card-default">
+            <div class="card-header">Welcome</div>
+            <div class="card-body">
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error placeat beatae facere aut consequatur reprehenderit earum unde ea eos adipisci natus molestias dolorem pariatur, consequuntur cupiditate possimus dolore fugiat quibusdam quisquam officia.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
